8842f4863532b5e8b734389cdf41f1a2c53a1c42
tamush95 <tamironen995@gmail.com>
2024-02-01 19:30:21 +0200

    first commit

---
+++ lesson1/script.js
@@ -0,0 +1,16 @@
+function welcomeMessage() {
+    alert("ברוכים הבאים לאתר שלי");
+}
+function showMessage() {
+    console.log("מציג הודעה");
+}
+function welcomeName() {
+    var name = document.getElementById("userName").value;
+
+    alert("ברוך הבא " + name);
+}
+function multiply50() {
+    var n = document.getElementById("num").value
+
+    alert(n * 50);
+}
---
+++ lesson2/script.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+function counter() {
+
+    document.getElementById("btn").innerHTML = ++count;
+}
+function multi() {
+    const n1 = +document.getElementById("num1").value;
+    const n2 = +document.getElementById("num2").value;
+
+    document.getElementById("output1").innerHTML = n1 * n2;
+
+}
+function plus() {
+    const n1 = +document.getElementById("num3").value;
+    const n2 = +document.getElementById("num4").value;
+
+    document.getElementById("output2").innerHTML = n1 + n2;
+
+}
+function plusResult() {
+    const n1 = +document.getElementById("num5").value;
+    const n2 = +document.getElementById("num6").value;
+    // const sum = n1 + n2;
+    // const result = n1 + " + " + n2 + " = " + sum;
+    const result = `${n1} + ${n2} = ${n1 + n2}`;
+
+    document.getElementById("output3").innerHTML = result;
+}
