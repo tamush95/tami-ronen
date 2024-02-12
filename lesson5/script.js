5762cbbccc969e9a97ec6dc08e133084ad56a3ca
tamarronen <tamironen995@gmail.com>
2024-02-12 19:48:17 +0200

    added

--- lesson5/script.js
+++ lesson5/script.js
@@ -6,6 +6,34 @@ function task1() {
         res += i + ', ';
         i += 2;
     }
+
     document.getElementById("output1").innerHTML = res;
     document.getElementById("btn").remove();
+}
+
+function task2(elem) {
+    const num = +elem.value;
+    let isPrime = true;
+
+    let i = 2;
+
+    while (i < num) {
+
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+
+        i++;
+    }
+
+    const output = document.getElementById("output2");
+
+    if (isPrime) {
+        output.innerHTML = 'ראשוני';
+        output.style.color = "blue";
+    } else {
+        output.innerHTML = 'פריק';
+        output.style.color = "green";
+    }
 }
